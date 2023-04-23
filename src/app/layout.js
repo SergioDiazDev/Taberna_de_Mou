84854c2d182789ee2fd4763d8f8509b9aa08467a
sdiaz-ru <SergioDiazDev@gmail.com>
2023-04-23 12:58:04 +0200

    Pequeños cambios

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,7 @@ export default function RootLayout({ children }) {
       <head>
         <title>Taberna de Mou</title>
         <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0"/>
       </head>
       <body>
       <Navigation />
