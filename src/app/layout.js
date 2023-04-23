bddf83a1c0360b746abece2a0a8acaf2b9b9ff0e
sdiaz-ru <SergioDiazDev@gmail.com>
2023-04-23 10:45:32 +0200

    Añadidos colores al menu

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,18 +4,18 @@ export const metadata = {
   title: 'Taberna de Mou',
   description: 'Mou al habla',
 }
- 
+
 export default function RootLayout({ children }) {
- return (
+  return (
     <html lang="es">
       <head>
         <title>Taberna de Mou</title>
         <meta charSet="utf-8" />
       </head>
       <body>
-        <Navigation />
-        {children}
-        </body>
+      <Navigation />
+      {children}
+      </body>
     </html>
   )
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,50 +1,60 @@
 import Link from 'next/link'
 import styles from './Navigation.module.css'
+import '../styles/globals.css'
 
-const links =[{
-  label: 'Home',
-  route: '/',
-},
-{
-  label: 'Futbol',
-  route: '/futbol',
-},
-{
-  label: 'F1',
-  route: '/f1',
-},
-{
-  label: 'Baloncesto',
-  route: '/baloncesto',
-},
-{
-  label: 'Series',
-  route: '/series',
-},
-{
-  label: 'Películas',
-  route: '/peliculas',
-},
-{
-  label: 'Varios',
-  route: '/varios',
-}
-]
 
-export  function Navigation() {
-   return  (
+const links = [
+  {
+    label: 'Home',
+    route: '/',
+    color: '#D0A9F5',
+  },
+  {
+    label: 'Futbol',
+    route: '/futbol',
+    color: 'var(--color-futbol)',
+  },
+  {
+    label: 'F1',
+    route: '/f1',
+    color: 'var(--color-f1)',
+  },
+  {
+    label: 'Baloncesto',
+    route: '/baloncesto',
+    color: 'var(--color-baloncesto)',
+  },
+  {
+    label: 'Series',
+    route: '/series',
+    color: 'var(--color-series)',
+  },
+  {
+    label: 'Peliculas',
+    route: '/peliculas',
+    color: 'var(--color-peliculas)',
+  },
+  {
+    label: 'Varios',
+    route: '/varios',
+    color: 'var(--color-varios)',
+  },
+];
+
+export function Navigation() {
+  return (
     <header className={styles.header}>
-          <nav>
-            <ul className={styles.navigation}>
-              {links.map(({ label, route }) => (
-                <li key={route}>
-                    <Link href={route}>
-                      {label}
-                    </Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </header>
+      <nav>
+        <ul className={styles.navigation}>
+          {links.map(({ label, route, color}) => (
+            <li key={route} style={{color}}>
+              <Link href={route}>
+                {label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
   );
 }
